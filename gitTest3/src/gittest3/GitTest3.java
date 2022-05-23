1d0e1a2f897bd69e944918e046950a387ab0fc6d
jamjamin <benjamjamin02@gmail.com>
2022-05-23 17:03:10 +1200

    commit doodyah

---
+++ gitTest3/src/gittest3/GitTest3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest3;
+
+/**
+ *
+ * @author zgg8595
+ */
+public class GitTest3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String dood = "dood";
+        String yah = "yah";
+        
+        System.out.println(dood + yah);
+        
+        
+    }
+    
+}
